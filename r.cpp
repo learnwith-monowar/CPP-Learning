4b5537c178b5ad47815f9db4dcc595d05349c3ff
learnwith-monowar <83909374+learnwith-monowar@users.noreply.github.com>
2021-05-12 19:58:25 +0600

    Add files via upload

---
+++ r.cpp
@@ -0,0 +1,52 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    string n;
+    cin >> n;
+    ll s = n.size();
+    ll a[s], g = s-1, p = s-1, k, min = 0, f = s, count = 0;
+    
+    if(s != 1) {
+        for(ll i = 0; i < s-1; i++) {
+            k = 1;
+            for(ll j = i+1; j < s; j++) {
+                if((n[i] == n[j]) && (k <= pow(10,6))) {
+                    k++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            while(g--) {
+                a[g] = k;
+                break;
+            }
+        }
+    }
+
+    if(s != 1) {
+        while(p--) {
+            if(min < a[p]) {
+                min = a[p];
+            }
+             else {
+                continue;
+            }
+        }
+    }
+    else {
+        min = 1;
+    }
+    
+    cout << min << endl;
+
+    return 0;
+}
